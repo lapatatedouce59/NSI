4a03186b949ce80d48227ee888e744601ce67f58
Amaury <momo29004@gmail.com>
2022-11-05 21:00:00 +0100

    Etablissement du script de vérification d'identité

--- scripts/index.js
+++ scripts/index.js
@@ -5,18 +5,31 @@
 
 (async ()=> {
     data=await(await fetch('../bdd.json')).json()
-    console.log(data)
+    //console.log(data)
 })()
 
 let data = {}
 
+currentUser=''
+currentUserRank=''
 
-
-
+//VERIFICATION DES CONDITIONS D'IDENTITE
 async function validate(){
     let usernameTxt = document.getElementById('USER').value
     let passwordTxt = document.getElementById('PASS').value
-    alert("Utilisateur "+usernameTxt+" et mot de passe "+passwordTxt)
+    if(checkUser(usernameTxt, passwordTxt)){
+        console.log('Compte trouvé! Rang: '+currentUserRank)
+    } else {console.log("Compte invalide!")}
 }
 
 
+//VERIFICATION DE L'EXISTANCE D'UN OBJECT
+function checkUser(user, pwd){
+    for (let dUser of data.users){
+        if (dUser.id===user && dUser.pwd===pwd && (dUser.rank)) {
+            currentUser=dUser.id
+            currentUserRank=dUser.rank
+            return true;
+        }
+    } return false;
+}
